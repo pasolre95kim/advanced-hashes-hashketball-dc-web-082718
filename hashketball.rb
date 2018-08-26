cbf9e876c10a56962d0b963edb6c794417773aa0
Michelle Kim <pasolre95kim@gmail.com>
2018-08-26 16:33:43 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -167,6 +167,17 @@ def player_numbers(team_name)
     end
   end
 end
+def player_stats(player_name)
+	game_hash.each do |location, team_data|
+		team_data[:players].each do |a|
+			if a[:player_name] == player_name
+				my_hash = a
+				my_hash.shift
+				return my_hash
+			end
+		end
+	end
+end
 
 def big_shoe_rebounds
 	#find largest shoe size
